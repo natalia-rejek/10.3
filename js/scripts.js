c07d5851ea63d95ff902aa89d69ef75711b1c203
Natalia <nettel@wp.pl>
2017-10-26 16:52:19 +0200

    second commit

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+var carouselList = $("#carousel ul");
+
+$(function(){
+	function changeSlides() {
+		carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide);
+	};
+	setInterval(changeSlides, 3000);
+	function moveFirstSlide() {
+		var firstItem = carouselList.find("li:first");
+		var lastItem = carouselList.find("li:last");
+
+		lastItem.after(firstItem);
+		carouselList.css({marginLeft:0});
+	};
+
+});
